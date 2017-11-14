8fe8a509d647242a9ea4d5af75f408ba66551b8c
JasonLu <sherlock_yang@qq.com>
2017-11-14 14:16:23 +0800

    添加python代码

---
+++ python/p1/helloworld.py
@@ -0,0 +1,17 @@
+print("Hello world!")
+
+name = 'luyang'
+name2 = name
+print(name2)
+
+name = "跑车哥"
+print("my name is ", name);
+
+'''print(name, name2);
+
+
+value = 2**10
+print(value)
+'''
+
+print("你好，世界!")
