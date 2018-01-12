1ebbb3ee7759ee5b6fa6aeefcc05bce6066354ab
JasonLu <sherlock_yang@qq.com>
2018-01-12 08:33:04 +0800

    第九天2

--- python/day9/ssh.py
+++ python/day9/ssh.py
@@ -4,10 +4,13 @@ import paramiko
 
 ssh = paramiko.SSHClient()
 
-ssh.connect(hostname='c1.salt.com', port=22, username='wupeiqi', password='123')
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+ssh.connect(hostname='10.0.031', port=52113, username='root', password='123')
 
 stdin, stdout, stderr = ssh.exec_command('df')
 
 result = stdout.read()
+print(result.decode())
 
 ssh.close()
