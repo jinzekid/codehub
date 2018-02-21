70fd7fa390ce81ac27b7d5bfa6863e7e3b3d9405
JasonLu <sherlock_yang@qq.com>
2018-02-21 22:45:14 +0800

    更新ProxyRequestUtil代码

--- python/lyutil/ly_ProxyRequestUtil.py
+++ python/lyutil/ly_ProxyRequestUtil.py
@@ -2,25 +2,40 @@
 import re, time, random
 import urllib.request
 
-proxies = {'http':'http://10.10.10.10:8765','https':'https://10.10.10.10:8765', 'http':'http://223.85.96.238:8123'}
+proxies = {'http':'http://10.10.10.10:8765',
+           'https':'https://10.10.10.10:8765',
+           'http':'http://223.85.96.238:8123'}
 
 class proxyrequest(object):
 
-    def __init__(self, url):
+    def __init__(self, url, saveCookie=False, openDebug=False):
         self.url        = url
+        self.savecookie = saveCookie
+        self.opendebug  = openDebug
 
     # 创建全局默认的opener对象
-    def do_installGlobalOpener(self):
+    def do_installGlobalOpener(self, b_saveCookie=False, b_openDebug=False):
         # proxy = urllib.request.ProxyHandler({'http': self.proxy_addr})
         proxy_support = urllib.request.ProxyHandler(proxies)
 
-        # # 开启DebugLog
-        # httphd = urllib.request.HTTPHandler(debuglevel=1)
-        # opener = urllib.request.build_opener(proxy, httphd)
-
         # 创建一个自定义的opener对象
-        opener = urllib.request.build_opener(proxy_support,
-                                             urllib.request.HTTPHandler)
+        # 开启DebugLog
+        if b_openDebug:
+            httphd = urllib.request.HTTPHandler(debuglevel=1)
+            opener = urllib.request.build_opener(proxy_support, httphd)
+        else:
+            opener = urllib.request.build_opener(proxy_support,
+                                                 urllib.request.HTTPHandler)
+
+        if b_saveCookie:
+            # 希望登录状态一直保持，使用Cookie处理
+            import http.cookiejar
+            # 使用http.cookiejar.CookieJar()创建CookieJar对象
+            cjar = http.cookiejar.CookieJar()
+            # 使用HTTPCookieProcessor创建cookie处理器，并以其为参数构建opener对象
+            opener.add_handler(urllib.request.HTTPCookieProcessor(cjar))
+
+
         # headers = ("User-Agent",
         #            "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Mobile Safari/537.36")
         headers = ("User-Agent",
@@ -34,10 +49,9 @@ class proxyrequest(object):
     # 自定义函数，功能为使用代理服务器
     def getdata(self):
         try:
-            import urllib.request
             # 创建全局默认的opener对象
             # 将opener安装为全局
-            self.do_installGlobalOpener()
+            self.do_installGlobalOpener(b_saveCookie=self.savecookie, b_openDebug=self.opendebug)
             data = urllib.request.urlopen(self.url).read().decode('utf-8')
             return data
         except urllib.error.HTTPError as e:
