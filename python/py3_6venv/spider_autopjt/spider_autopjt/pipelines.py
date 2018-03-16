9537f97722f7d347168b27677b23a0350132a372
JasonLu <sherlock_yang@qq.com>
2018-03-16 17:32:31 +0800

    spider_mysqlpjt

--- python/py3_6venv/spider_autopjt/spider_autopjt/pipelines.py
+++ python/py3_6venv/spider_autopjt/spider_autopjt/pipelines.py
@@ -7,12 +7,20 @@
 
 import codecs
 import json
+import pymysql
 
 class SpiderAutopjtPipeline(object):
 
     def __init__(self):
         print("init_spider")
         self.file = codecs.open('mydata.json', 'wb', encoding='utf-8')
+        print("init_database")
+        self.conn = pymysql.connect(host='127.0.0.1', user='root', passwd='',
+                                 db='mypydb')
+        # 通过cursor创建游标
+        self.cursor = self.conn.cursor()
+
+        print("end init")
 
     def process_item(self, item, spider):
         print("processing...")
@@ -26,11 +34,29 @@ class SpiderAutopjtPipeline(object):
 
             i = json.dumps(dict(goods), ensure_ascii=False)
             line = i + '\n'
-
             self.file.write(line)
+
+
+            # 保存数据库
+            # 构造对应的sql语句
+            sql = "insert into mydangdangtb(name, price, link, comnum) " \
+                  "values" \
+                  "('" + name + "','" + price + "','"+ link + "'," \
+                                                              "'" +comnum+"');"
+            print("=====执行sql语句=====")
+            print(sql)
+
+            self.cursor.execute(sql)
+            # # 通过query实现执行对应的sql语句
+            # affect_rows = self.conn.query(sql)
+            # print("影响 " + affect_rows + ' 行')
+        self.conn.commit()
         return item
 
     # close_spider() 方法一般在关闭蜘蛛时调用
     def close_spider(self, spider):
         print("close_spider")
         self.file.close()
+        self.cursor.close()
+        self.conn.close()
+
