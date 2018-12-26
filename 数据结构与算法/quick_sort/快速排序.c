9794ab8c58e43812920ffba35ab6edaea1217b4a
JasonLu <8287225+jinzekid@users.noreply.github.com>
2018-12-26 11:01:54 +0800

    update:测试快速排序

--- 数据结构与算法/quick_sort/快速排序.c
+++ 数据结构与算法/quick_sort/快速排序.c
@@ -13,6 +13,7 @@ void swap(int a[], int i, int j) {
 }
 
 // 快速排序
+/*
 void quick_sort(int a[], int low, int high) {
 	int left, right, tmp;
 	if (low >= high) return;
@@ -41,26 +42,55 @@ void quick_sort(int a[], int low, int high) {
 	quick_sort(a, low, left-1);
 
 }
+*/
 
+void quick_sort(int a[], int low, int high) {
+
+	if (low > high) return;
+
+	int i, j;
+	int t = a[low];
+
+	i = low;
+	j = high;
+	while(i<j) {
+		while (a[j]>=t && i<j)
+			j--;
+		
+		while(a[i]<=t && i<j)
+			i++;
+		
+		if (i<j) {
+            printf("swap:i=%d,j=%d\n", i, j);
+			swap(a, i, j);
+		}
+	}
+		
+	
+	// 交换基数值
+	swap(a , low, i);
+	quick_sort(a, low, i-1);
+	quick_sort(a, i+1, high);
+}
 
 int main() {
 
 	int i;
 	scanf("%d", &n);
 
-	for (i = 1; i <= n; i++) {
+	for (i = 0; i < n; i++) {
 		scanf("%d", &a[i]);
 	}
 	
 	printf("排序前数据:\n");
-	for (i = 1; i <= n; i++) {
+	for (i = 0; i < n; i++) {
 		printf("%d ",a[i]);
 	}
 	printf("\n");
 
-	quick_sort(a, 1, n);
+	quick_sort(a, 0, n-1);
 	printf("排序后数据(从大到小):\n");
-	for (i = 1; i <= n; i++) {
+	for (i = 0; i < n; i++) {
 		printf("%d ", a[i]);
 	}
 	printf("\n");
