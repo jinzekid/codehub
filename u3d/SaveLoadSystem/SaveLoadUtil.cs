01dd1b776b6eedcf539b47f2a5343b0b0aba3fdd
Jason Lu <514432996@qq.com>
2019-04-10 14:07:05 +0800

    Update SaveLoadUtil.cs

--- u3d/SaveLoadSystem/SaveLoadUtil.cs
+++ u3d/SaveLoadSystem/SaveLoadUtil.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 
 public class SaveLoadUtil : MonoBehaviour
 { 
+    // 保存数据
     public void SaveData()
     {
         Player01 player = new Player01() { level = 10, health = 150, sex = true };
         SaveSystem.SaveGameData(player, "player.fun");
     }
 
+    // 加载数据
     public void LoadData()
     {
         Player01 player = SaveSystem.LoadGameData("player.fun") as Player01;
